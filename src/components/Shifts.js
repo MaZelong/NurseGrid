8b6ff88f970b20a935ce889622614a80682dbca0
MaZelong <zelongma1991@gmail.com>
2016-04-03 12:16:07 -0700

    add icon based on time

--- src/components/Shifts.js
+++ src/components/Shifts.js
@@ -3,6 +3,9 @@ import Router from 'react-router';
 import Shift from './SingleShift';
 import List from 'material-ui/lib/lists/list';
 import ListItem from 'material-ui/lib/lists/list-item';
+import Card from 'material-ui/lib/card/card';
+import CardTitle from 'material-ui/lib/card/card-title';
+import CardText from 'material-ui/lib/card/card-text';
 
 
 import $ from 'jquery';
@@ -49,32 +52,34 @@ export default class Shifts extends React.Component {
 
   render() {
     return (
-      <List>
-        {this.state.shifts.map(function(shift, i) {
-          var boundClick = this.handleClick.bind(this, i);
-          return (
-            <Shift onClick={boundClick} key={i} data={shift.hospitalName} />
-          );
-        }, this)}
-      </List>
-    )
-  }
-
-  renderItem(item, index) {
-    //let that = this;
-    let boundClick = this.handleClick.bind(this, index, item);
-    return (
-      <Shift
-        onClick={boundClick}
-        key = {index}
-        data={item.hospitalName} />
+      <Card>
+        <CardTitle title="NurseGrid XXXX" />
+        <List>
+          {this.state.shifts.map(function(shift, i) {
+            let boundClick = this.handleClick.bind(this, i);
+            let shiftWorkSiteId = shift.worksiteId;
+            let workSite = this.state.worksites.filter((worksite, index) => {
+              return worksite.worksiteId === shift.worksiteId && worksite.hospitalId === shift.hospitalId;
+            })[0];
+            return (
+              <CardText>
+              <Shift 
+                onClick={boundClick} 
+                key={i}
+                shift={shift}
+                workSite={workSite} />
+                </CardText>
+            );
+          }, this)}
+        </List>
+      </Card>
     )
   }
 
   handleClick(index, item) {
     console.log("kdalijfdowijeowiqo");
     console.log(this.context);
-    this.props.history.pushState(null, "/" + this.state.userId + "/shifts/" + this.state.shifts[index].shiftId);
+    //this.props.history.pushState(null, "/" + this.state.userId + "/shifts/" + this.state.shifts[index].shiftId);
   }
 
 
--- src/components/SingleShift.js
+++ src/components/SingleShift.js
@@ -3,10 +3,70 @@ import ListItem from 'material-ui/lib/lists/list-item';
 import Avatar from 'material-ui/lib/avatar';
 import styles from 'material-ui/lib/styles';
 import InlineSVG from 'svg-inline-react';
+import moment from 'moment';
 
 const colors = styles.Colors;
 
 export default class UserProfile extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      departmentName: props.workSite.departmentName,
+      hospitalName: props.workSite.hospitalName,
+      startTime: props.shift.start,
+      endTime: props.shift.end,
+      timeZone: props.shift.timeZone,
+      shiftTypeId: props.shift.shiftTypeId
+    }
+  }
+
+  componentDidMount() {
+    this.handleTime();
+    this.handleShiftType();
+    this.handleShiftIcon();
+  }
+
+  handleTime() {
+    const startDate = moment(this.state.startTime);
+    const endDate = moment(this.state.endTime);
+    this.setState({
+      startDate: startDate.format("MMM-Do"),
+      start: startDate.format("HH:mm"),
+      end: endDate.format("HH:mm")
+    });
+  }
+
+  handleShiftType() {
+    const shiftTypes = {
+      0: 'Regular Shift',
+      1: 'On Call',
+      2: 'Vacation',
+      3: 'Education',
+      4: 'Meeting',
+      5: 'Payday',
+      64: 'Available To Work'
+    };
+    this.setState({
+      shiftType: shiftTypes[this.state.shiftTypeId]
+    });
+  }
+
+  handleShiftIcon() {
+    let src = ''
+    if (moment(this.state.startTime).hour() >= 6 && moment(this.state.startTime).hour() < 12) {
+      src = require('../icons/DayShift.svg');
+    }
+    if (moment(this.state.startTime).hour() >= 12 && moment(this.state.startTime).hour() < 17) {
+      src = require('../icons/SwingShift.svg');
+    }
+    if (moment(this.state.startTime).hour() >= 17) {
+      src = require('../icons/NightShift.svg');
+    }
+    this.setState({
+      iconSrc: src
+    });
+  }
 
 	render() {
 		return (
@@ -14,20 +74,28 @@ export default class UserProfile extends React.Component {
         leftAvatar={
           <Avatar
           icon = {<InlineSVG 
-            src={require('../icons/DayShift.svg')}
+            src={this.state.iconSrc}
           />}
           backgroundColor={colors.transparent}
           />
         }
         onClick={this.props.onClick}
-      >
-        {this.props.data}
+        primaryText={
+          <p>
+            <span style={{color: colors.blue500}}>{this.state.startDate}</span><br/>
+            <span>{this.state.start} To {this.state.end}</span>
+          </p>
+        }
+        secondaryText={
+          <p>
+            <span style={{color: colors.grey500}}>{this.state.shiftType}</span><br/>
+            {this.props.workSite.hospitalName}
+            {this.props.workSite.departmentName}
+          </p>
+        }
+      > 
       </ListItem>
 		)
 	}
 
-  // handleClick() {
-  //   console.log("kdalijfdowijeowiqo");
-  //   console.log(this.props);
-  // }
 }
